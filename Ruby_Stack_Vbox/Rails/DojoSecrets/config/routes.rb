85b93a63ad03a3266f4efd1f112e989f6edaca6d
Nick Graham <tsuchimiya@Nicks-Air.home>
2020-06-17 13:12:19 -0500

    Rails2

---
+++ Ruby_Stack_Vbox/Rails/DojoSecrets/config/routes.rb
@@ -0,0 +1,33 @@
+Rails.application.routes.draw do
+  get 'likes/create'
+
+  get 'likes/destroy'
+
+  get 'secrets/index'
+
+  root 'sessions#new'
+
+  get 'secrets/index'
+
+  get 'users/new'
+  post 'users/new' => 'users#create'
+
+  get 'users/:id' => 'users#show'
+
+  get 'users/:id/edit' => 'users#edit'
+  patch 'users/:id/edit' => 'users#update'
+
+  delete 'users/:id' => 'users#delete'
+  #---------------------------------------
+  get 'sessions/new'
+  post 'sessions/new' => 'sessions#create'
+  
+  delete 'sessions/:id' => 'sessions#destroy'
+  #---------------------------------------
+  post 'secrets/new' => 'secrets#create'
+  delete 'secrets/:id' => 'secrets#delete' 
+  get 'secrets' => 'secrets#index'
+  #---------------------------------------
+  post 'likes/:id' => 'likes#create'
+  delete 'likes/:id' => 'likes#delete'
+end
